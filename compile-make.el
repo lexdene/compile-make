1d2621edb744f5339d44caac348db79f7e0fb09b
Elephant Liu <lexdene@sohu.com>
2014-12-05 11:34:13 +0800

    add compile-make-ex to select target

--- compile-make.el
+++ compile-make.el
@@ -6,14 +6,20 @@
   "get the parent directory"
   (file-name-directory (directory-file-name path)))
 
-(defun compile-make ()
-  "run make to compile"
-  (interactive)
+
+(defun compile-make-ex (target)
+  "run make to compile. provide more arguments."
+  (interactive "starget: ")
   (let ((path (file-name-directory buffer-file-name))
         (go-on t))
     (while go-on
       (if (file-readable-p (concat path "Makefile"))
-          (compilation-start (format "cd %s && make" path)))
+          (compilation-start (format "cd %s && make %s" path target)))
       (setq path (get-parent-directory path))
       (if (equal path "/")
           (setq go-on nil)))))
+
+(defun compile-make ()
+  "run make to compile"
+  (interactive)
+  (compile-make-ex ""))
